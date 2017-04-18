154581505a7f2b6d71745ac2998b7d23615d2950
Ali Schlereth <aschlere@sbcglobal.net>
2017-04-17 22:16:33 -0600

    Delete puts driven development

--- binary_search.rb
+++ binary_search.rb
@@ -38,27 +38,3 @@ class BinarySearch
   end
 
 end
-
-numbers = [1,3,5,7,9]
-search = BinarySearch.new(numbers)
-result1 = search.is_present?(7)
-result2 = search.is_present?(3)
-result3 = search.is_present?(2)
-result4 = search.is_present?(1)
-result5 = search.is_present?(4)
-result6 = search.is_present?(9)
-
-puts result1
-puts result2
-puts result3
-puts result4
-puts result5
-puts result6
-
-letters = ["a", "b", "f", "y"]
-letter_search = BinarySearch.new(letters)
-result7 = letter_search.is_present?("b")
-result8 = letter_search.is_present?("c")
-result9 = letter_search.is_present?("t")
-puts result7
-puts result8
